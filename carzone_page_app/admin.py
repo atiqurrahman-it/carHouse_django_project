73fc7bd64388a50b092b97d44b5cc90a45d20c5f
atiqur <atiqur@gmail.com>
2021-02-18 14:56:09 +0600

    Completed Teams Model

--- carzone_page_app/admin.py
+++ carzone_page_app/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from .models import Teams
 
+from django.utils.html import format_html
+
 
 # Register your models here.
 
 class TeamsAdmin(admin.ModelAdmin):
     class Meta:
         model = Teams
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="40" style="border-radius:50%" />'.format(obj.photo.url))
+
+    image_tag.short_description = 'photo'
+
+    list_display = ['id', 'image_tag', 'first_name', 'designation', 'create_data']
+    list_display_links = ('id','image_tag', 'first_name')
+    search_fields = ['create_data', 'first_name', 'last_name']
+    list_filter = ['create_data', 'first_name']
 
 
 admin.site.register(Teams, TeamsAdmin)
--- carzone_page_app/views.py
+++ carzone_page_app/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Teams
+
 
 # Create your views here.
 
 def Home_page(request):
-    return render(request, 'pages/index.html')
+    total_team = Teams.objects.all()
+    data = {
+        "total_team": total_team,
+    }
+    return render(request, 'pages/index.html', data)
 
 
 def About_page(request):
-    return render(request, 'pages/about.html')
+    total_team = Teams.objects.all()
+    data = {
+        "total_team": total_team,
+    }
+    return render(request, 'pages/about.html', data)
+
 
 def Services_page(request):
     return render(request, 'pages/services.html')
+
 
 def Contact_page(request):
     return render(request, 'pages/contact.html')
